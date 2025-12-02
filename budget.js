d77cb4152bdac13e9dbd64f33786f874195e96f7
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2025-12-02 13:00:51 -0500

    Feat: added forms to HTML and WIP on dynamically switching forms

--- budget.js
+++ budget.js
@@ -54,3 +54,55 @@ class Budget {
 }
 
 module.exports = Budget;
+
+const user = new Budget(
+  'Jane',
+  {
+    income1: {
+      description: 'Full-Time Job',
+      amount: 2115.38
+    },
+    income2: {
+      description: 'Weekend Job',
+      amount: 200.02
+    }
+  },
+  {
+    expense1: {
+      description: 'Rent',
+      amount: 1509.25,
+    },
+    expense2: {
+      description: 'Utilities',
+      amount: 105.50
+    },
+    expense3: {
+      description: 'Groceries',
+      amount: 520.25
+    }
+  }
+);
+
+const addIncomeSec = document.querySelector('.main__add-property--income');
+const addExpenseSec = document.querySelector('.main__add-property--expense');
+
+const formSelectBtn = document.querySelectorAll('.form__btn');
+
+formSelectBtn.forEach( btn => {
+  btn.addEventListener('click', function() {
+    formSelectBtn.forEach( b => {
+      b.classList.remove('form__btn--selected');
+    })
+    this.classList.add('form__btn--selected');
+  })
+});
+
+function displayFormSec(choice) {
+  if (choice === 'income') {
+    addIncomeSec.classList.remove('hidden');
+    addExpenseSec.classList.add('hidden');
+  } else {
+    addIncomeSec.classList.add('hidden');
+    addExpenseSec.classList.remove('hidden');
+  }
+}
